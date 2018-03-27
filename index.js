d409d584f3e68a99a8159eb72171973260982051
jchuajy <j.chua.jy@gmail.com>
2018-03-27 22:52:25 +0800

    HW done

--- index.js
+++ index.js
@@ -2,16 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const exphbs = require('express-handlebars');
-const { Client } = require('pg');
+const {
+  Client
+} = require('pg');
+const {
+  Pool
+} = require('pg');
+
 
 // Initialise postgres client
 const client = new Client({
-  user: 'akira',
+  user: 'postgres',
   host: '127.0.0.1',
   database: 'pokemons',
   port: 5432,
 });
 
+const pool = new Pool({
+  user: 'postgres',
+  host: '127.0.0.1',
+  database: 'pokemons',
+  port: 5432,
+});
 /**
  * ===================================
  * Configurations and set up
@@ -21,7 +33,9 @@ const client = new Client({
 // Init express app
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(methodOverride('_method'));
 
 
@@ -29,6 +43,12 @@ app.use(methodOverride('_method'));
 app.engine('handlebars', exphbs.create().engine);
 app.set('view engine', 'handlebars');
 
+// the pool with emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+});
 
 /**
  * ===================================
@@ -36,22 +56,170 @@ app.set('view engine', 'handlebars');
  * ===================================
  */
 
-app.get('/', (req, res) => {
-  // query database for all pokemon
+//go to home page
+function goToHome(request, response) {
+  //connect to db
+  pool.connect((err, client, done) => {
+    //error logs
+    if (err) console.error(err);
+    // your queries go here
+    let queryString = "SELECT * FROM pokemon";
+    // your dynamic values go here (i.e what does $1 $2 $3 represent. use the obj returned from readFile)
+    let values = [];
+    //send the query to the database
+    client.query(queryString, values, (err2, res) => {
+      done();
+      if (err2) {
+        //if error, send msg
+        console.log('query error', err2.message);
+      } else {
+        //else show the result of the first row
+        console.log('result', res.rows[0]);
+        //write the context based on the result of the sql query
+        let context = {
+          pokemon: res.rows,
+        };
+        //render the home.handlebars with the context stated.
+        response.render("home", context);
+      };
+    });
+  });
+};
 
-  // respond with HTML page displaying all pokemon
-});
+function showPokemonByPriId(request, response) {
+  pool.connect((err, client, done) => {
+    //error logs
+    if (err) console.error(err);
+    // your queries go here
+    //get query string based on the request.params.id
+    let queryString = "SELECT * FROM pokemon WHERE id = " + request.params.id;
+    // your dynamic values go here (i.e what does $1 $2 $3 represent. use the obj returned from readFile)
+    let values = [];
+    //send the query to the database
+    client.query(queryString, values, (err2, res) => {
+      done();
+      if (err2) {
+        //if error, send msg
+        console.log('query error', err2.message);
+      } else {
+        //write the context based on the result of the sql query
+        let context = {
+          pokemon: res.rows[0],
+        };
+        //render the home.handlebars with the context stated.
+        response.render("pokemon", context);
+      };
+    });
+  });
+};
 
-app.get('/new', (request, response) => {
-  // respond with HTML page with form to create new pokemon
-  response.render('new');
-});
+
+
+function goToCreationForm(request, response) {
+  //respond with a rendering of form.handlebars
+  response.render("new");
+};
+
+function newPokemonEntry(request, response) {
+  //request.body refers to the form data submitted
+  pool.connect((err, client, done) => {
+    //error logs
+    if (err) console.error(err);
+    // your queries go here
+    //get query string based on the form data submitted
+    let queryString = "INSERT INTO pokemon (num, name, img, weight, height) VALUES ($1, $2, $3, $4, $5)";
+    // your dynamic values go here (i.e what does $1 $2 $3 represent. use the obj returned from readFile)
+    let values = [request.body.num, request.body.name, request.body.img, request.body.weight, request.body.height];
+
+    client.query(queryString, values, (err2, res) => {
+      done();
+      if (err2) {
+        //if error, send msg
+        console.log('query error', err2.message);
+      } else {
+        response.redirect("/");
+      };
+    });
+  });
+};
+
+function editById(request, response) {
+  pool.connect((err, client, done) => {
+    //error logs
+    if (err) console.error(err);
+    // your queries go here
+    //get query string based on the request.params.id
+    let queryString = "SELECT * FROM pokemon WHERE id = " + request.params.id;
+    // your dynamic values go here (i.e what does $1 $2 $3 represent. use the obj returned from readFile)
+    let values = [];
+    client.query(queryString, values, (err2, res) => {
+      done();
+      if (err2) {
+        //if error, send msg
+        console.log('query error', err2.message);
+      } else {
+        //write the context based on the result of the sql query
+        let context = {
+          pokemon: res.rows[0],
+        };
+        //render the home.handlebars with the context stated.
+        response.render("edit", context);
+      };
+    });
+  });
+};
+
+function saveEdits(request, response) {
+  pool.connect((err, client, done) => {
+    //error logs
+    if (err) console.error(err);
+    // your queries go here
+    //get query string based on the request.params.id and insert into it
+    let queryString = "UPDATE pokemon SET num = $1, name = $2, img = $3, weight = $4, height = $5 WHERE id = " + request.params.id;
+    // your dynamic values go here (i.e what does $1 $2 $3 represent. use the obj returned from readFile)
+    let values = [request.body.num, request.body.name, request.body.img, request.body.weight, request.body.height];
+    client.query(queryString, values, (err2, res) => {
+      done();
+      if (err2) {
+        //if error, send msg
+        console.log('query error', err2.message);
+      } else {
+        //redirect to home page
+        response.redirect("/" + request.params.id);
+      };
+    });
+  });
+};
+
+function deletePokemonById(request, response) {
+  pool.connect((err, client, done) => {
+    //error logs
+    if (err) console.error(err);
+    // your queries go here
+    //get query string based on the request.params.id and delete it
+    let queryString = "DELETE from pokemon WHERE id = " + request.params.id;
+    // your dynamic values go here (i.e what does $1 $2 $3 represent. use the obj returned from readFile)
+    let values = [];
+    client.query(queryString, values, (err2, res) => {
+      done();
+      if (err2) {
+        //if error, send msg
+        console.log('query error', err2.message);
+      } else {
+        //redirect to home page
+        response.redirect("/");
+      };
+    });
+  });
+};
+
+
 
 
 app.post('/pokemon', (req, response) => {
   let params = req.body;
 
-  const queryString = 'INSERT INTO pokemon(name, height) VALUES($1, $2)'
+  const queryString = 'INSERT INTO pokemon (name, height) VALUES($1, $2)'
   const values = [params.name, params.height];
 
   client.connect((err) => {
@@ -71,6 +239,16 @@ app.post('/pokemon', (req, response) => {
   });
 });
 
+app.get("/", goToHome);
+app.get("/new", goToCreationForm);
+app.get("/:id/edit", editById);
+app.get("/:id", showPokemonByPriId);
+
+app.post("/", newPokemonEntry);
+
+app.put("/:id", saveEdits);
+
+app.delete("/:id", deletePokemonById);
 
 /**
  * ===================================
